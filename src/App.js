e33a21a25692e60c6346e6818144e54428e90a72
Robbie Smith <robert.smith160@icloud.com>
2017-04-05 13:42:10 -0600

    working on meta-mask

--- src/App.js
+++ src/App.js
@@ -128,8 +128,7 @@ class App extends Component {
       } else {
         console.log('No web3? You should consider trying MetaMask!');
           // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-          window.Web3 = new Web3(new Web3.providers.HttpProvider(
-            "http://localhost:8545"));
+          window.Web3 = new Web3(new Web3.providers.HttpProvider( "http://localhost:8545"));
       }
     });
   }
